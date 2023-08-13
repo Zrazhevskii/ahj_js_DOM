bb863f851adf00ca8151bb25186cd3adfc0c29e6
Sanshai <vbn-134@yandex.ru>
2023-08-13 13:51:47 +0300

    коррекция стилей

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  const goblin = document.querySelectorAll('.game-box')
+  console.log(goblin[0]);
 })
---
+++ src/js/game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor(elem) {
+    this.elem = elem
+  }
+
+  chanch
+
+}
